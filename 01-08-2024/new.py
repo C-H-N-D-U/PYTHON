0b02f6d5625bf4c113295fecbdffe32b3b72b928
C-H-N-D-U <chandrakanttnetaam@outlook.com>
2024-08-02 07:51:09 +0530

    new adding

---
+++ 01-08-2024/new.py
@@ -0,0 +1,9 @@
+# Data types
+
+# String
+# print("Hello"[0-2])
+# num_char = len(input("What is your Name ?"))
+# print(num_char)
+two_digit_number = input("Enter any number : ")
+print( "Your enter data type is " + int(type(two_digit_number)))
+
